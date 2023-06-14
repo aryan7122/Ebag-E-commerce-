c2a5b159b4516208de82b86599dad0b85f0039e3
Aryan <aryankushwahalifenote@gmail.com>
2023-06-14 20:30:02 +0530

    1.1.1

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useContext } from "react";
 import "./Home.scss";
 import Banner from './Banner/Banner'
 import Category from './Category/Category'
 import Products from '../Products/Products'
+import { fetchDataFromApi } from '../../utils/api'
+import { Context } from "../../utils/context";
 const Home = () => {
+    const { categories, setCategories } = useContext((Context));
+
+    useEffect(() => {
+        getCategories();
+    }, [])
+    const getCategories = () => {
+        fetchDataFromApi("/api/categories?populate=*").then((res) =>{
+            console.log(res);
+            setCategories(res);
+       } );
+    };
+
     return (
         <div className="home">
             <Banner />
             <div className="main_content">
                 <div className="layyout">
-                    <Category />
+                    <Category categories={categories} />
                     <Products headingText="Popular Products" />
                 </div>
             </div>
